fb1841096516130d3f127e85d17a7b6e6fe20e6f
sibadima <148964526+sibadima@users.noreply.github.com>
2025-07-27 21:14:02 +0200

    Update CreditAccount.java

--- CreditAccount.java
+++ CreditAccount.java
@@ -2,37 +2,27 @@
 //указанного в конструкторе, но не может уходить в плюс.
 
 public class CreditAccount extends Account {
-    private long creditLimit;
+    private long limit;
 
-    //конструктор
-    public CreditAccount(long balance, long creditLimit) {
-        super(balance);
-        this.creditLimit = creditLimit;
+    public CreditAccount(long limit) {
+        this.limit = limit;
     }
 
-    //покупка со счета, переопределяем метод
+    @Override
+    public boolean add(long amount) {
+        if (balance + amount <= 0) {
+            balance += amount;
+            return true;
+        }
+        return false;
+    }
+
     @Override
     public boolean pay(long amount) {
-        if (amount <= 0) {
-            return false; //нельзя оплатить отрицательную сумму или ноль
-        }
-        if (balance - amount >= -creditLimit) { //проверка превышения кредитного лимита
+        if(balance - amount >= limit) {
             balance -= amount;
             return true;
         }
-        return  false; //возвращаем false если кредитный лимит превышен
-    }
-
-    //пополнение счета
-    @Override
-    public boolean add (long amount) {
-        if (amount <= 0) {
-            return false; //нельзя пополнить на отриц. сумму или на ноль
-        }
-        if (balance + amount > 0) {
-            return false; //по условию счет должен быть меньше или равен 0
-        }
-        balance += amount;
-        return true;
+        return false;
     }
 }
